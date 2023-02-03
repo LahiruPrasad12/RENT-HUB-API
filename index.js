eb07518ab92647163085802b2cedc1d117dc4f56
LahiruPrasad12 <lahirupr471@gmail.com>
2023-02-03 21:07:39 +0530

    changes

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const exp = require("./server");
+const port = 3000;
+exp.use(express.json());
+mongoose.set("strictQuery", false);
+
+const BASE_URL =
+  "mongodb+srv://LahiruPrasad12:Lahiru12@cluster0.n1wciok.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(BASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(
+    async () => {
+      console.log("Rent-Hub DB Connected");
+    },
+    (err) => {
+      console.log(err, "Error");
+    }
+  );
+
+exp.listen(port, () => {
+  console.log(`Rent-Hub Server Started..`);
+});
+
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+var cors = require('cors');
+
+
+app.use(
+    cors({
+        origin: '*',
+        credentials: true,
+    })
+);
+const bodyPaser = require('body-parser')
+app.use(bodyPaser.json())
+app.use(express.urlencoded({ extended: true }))
+
+const User = require("./routes/User-route");
+const Item = require("./routes/Item-route");
+const Rent = require("./routes/Rent-route");
+
+app.use("/user",User);
+app.use("/item",Item);
+app.use("/rent",Rent);
+
+
+
+module.exports = app;
